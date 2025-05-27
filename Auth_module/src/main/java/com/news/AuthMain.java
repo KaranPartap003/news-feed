92d84d7be025f9919c9276ffedbcc7c0a0c83ee0
karanPartap003 <karanpartap003@gmail.com>
2025-05-27 22:19:36 +0530

    Added DTOs for better mapping

--- Auth_module/src/main/java/com/news/AuthMain.java
+++ Auth_module/src/main/java/com/news/AuthMain.java
@@ -3,7 +3,6 @@ package com.news;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Authentication {
     public static void main(String[] args) {
---
+++ Auth_module/src/main/java/com/news/Service/JWTservice.java
@@ -0,0 +1,2 @@
+package com.news.Service;public class JWTservice {
+}
